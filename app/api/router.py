7f73f94afe4ca422f9b8aec32ee800f5a1470f43
bagelsrgood4me <joejohnson417@gmail.com>
2024-03-22 14:50:41 -0500

    Activate all apis

--- app/api/router.py
+++ app/api/router.py
@@ -11,40 +11,40 @@ router = Router()
 # region Local
 
 
-# @router.get("/single_local")
-# def single_local(request):
-#     res = add.si(1,1,30).apply_async()
-#     return res.id
-#
-# @router.get("/single_local_retrieve")
-# def single_local_retrieve(request, id: str):
-#     res = AsyncResult(id)
-#     if res.ready():
-#         return res.get()
-#     return f"{id}: Processing"
+@router.get("/single_local")
+def single_local(request):
+    res = add.si(1,1,30).apply_async()
+    return res.id
+
+@router.get("/single_local_retrieve")
+def single_local_retrieve(request, id: str):
+    res = AsyncResult(id)
+    if res.ready():
+        return res.get()
+    return f"{id}: Processing"
 
 
-# @router.get("/multi_group_local")
-# def multi_group_local(request):
-#     task_group = group([add.si(x[0], x[1], x[2]) for x in [(1,1, 10), (2,2, 5), (3,3, 3)]])
-#     res = task_group.apply_async()
-#     res.save()
-#     return res.id
-#
-#
-# @router.get("/multi_group_local_retrieve")
-# def multi_group_local_retrieve(request, id: str):
-#     res = GroupResult.restore(id)
-#
-#     if res.waiting():
-#         return f"{id}: Waiting"
-#     if not res.ready():
-#         return f"{id}: Processing"
-#     if res.failed():
-#         return f"{id}: Failed"
-#     if not res.successful():
-#         return f"{id}: Completed {res.completed_count()}"
-#     return res.join()
+@router.get("/multi_group_local")
+def multi_group_local(request):
+    task_group = group([add.si(x[0], x[1], x[2]) for x in [(1,1, 10), (2,2, 5), (3,3, 3)]])
+    res = task_group.apply_async()
+    res.save()
+    return res.id
+
+
+@router.get("/multi_group_local_retrieve")
+def multi_group_local_retrieve(request, id: str):
+    res = GroupResult.restore(id)
+
+    if res.waiting():
+        return f"{id}: Waiting"
+    if not res.ready():
+        return f"{id}: Processing"
+    if res.failed():
+        return f"{id}: Failed"
+    if not res.successful():
+        return f"{id}: Completed {res.completed_count()}"
+    return res.join()
 
 
 @router.get("/multi_chord_local")
@@ -65,43 +65,43 @@ def multi_chord_local_retrieve(request, id: str):
 # endregion
 
 # region Remote
-# @router.get("/single_remote")
-# def single_remote(request):
-#     task = 'opendns.tasks.block'
-#     sig = signature(task, args=(1,1), immutable=True, app=celery_app)
-#     res = sig.apply_async()
-#     return res.id
-#
-# @router.get("/single_remote_retrieve")
-# def single_remote_retrieve(request, id: str):
-#     res = AsyncResult(id)
-#     if res.ready():
-#         return res.get()
-#     return f"{id}: Processing"
-#
-#
-# @router.get("/multi_group_remote")
-# def multi_group_remote(request):
-#     task = 'opendns.tasks.block'
-#     task_group = group([signature(task, args=(x[0], x[1]), immutable=True, app=celery_app) for x in [(1,1), (2,2), (3,3)]])
-#     res = task_group.apply_async()
-#     res.save()
-#     return res.id
-#
-#
-# @router.get("/multi_group_retrieve_remote")
-# def multi_group_retrieve_remote(request, id: str):
-#     res = GroupResult.restore(id)
-#
-#     if res.waiting():
-#         return f"{id}: Waiting"
-#     if not res.ready():
-#         return f"{id}: Processing"
-#     if res.failed():
-#         return f"{id}: Failed"
-#     if not res.successful():
-#         return f"{id}: Completed {res.completed_count()}"
-#     return res.join()
+@router.get("/single_remote")
+def single_remote(request):
+    task = 'opendns.tasks.block'
+    sig = signature(task, args=(1,1), immutable=True, app=celery_app)
+    res = sig.apply_async()
+    return res.id
+
+@router.get("/single_remote_retrieve")
+def single_remote_retrieve(request, id: str):
+    res = AsyncResult(id)
+    if res.ready():
+        return res.get()
+    return f"{id}: Processing"
+
+
+@router.get("/multi_group_remote")
+def multi_group_remote(request):
+    task = 'opendns.tasks.block'
+    task_group = group([signature(task, args=(x[0], x[1]), immutable=True, app=celery_app) for x in [(1,1), (2,2), (3,3)]])
+    res = task_group.apply_async()
+    res.save()
+    return res.id
+
+
+@router.get("/multi_group_retrieve_remote")
+def multi_group_retrieve_remote(request, id: str):
+    res = GroupResult.restore(id)
+
+    if res.waiting():
+        return f"{id}: Waiting"
+    if not res.ready():
+        return f"{id}: Processing"
+    if res.failed():
+        return f"{id}: Failed"
+    if not res.successful():
+        return f"{id}: Completed {res.completed_count()}"
+    return res.join()
 
 
 @router.get("/multi_chord_remote")
